006aa33cb22226366b63f9b948af088f08359692
maidinh2409 <134086665+maidinh2409@users.noreply.github.com>
2025-07-02 22:17:07 -0700

    Unit Test
    -- Update unit test for raw nasa db

--- models/raw/raw_nasa_db.sql
+++ models/raw/raw_nasa_db.sql
@@ -16,26 +16,26 @@ spectype_cleaned as (
 
 select
     concat(
-        UPPER(replace(replace(replace(pl_name, '+', ''), '-',''), ' ', '')), 
-        UPPER(replace(replace(replace(hostname, '+', ''), '-',''), ' ', ''))
+        UPPER(replace(replace(replace(ifnull(pl_name, ''), '+', ''), '-',''), ' ', '')), 
+        UPPER(replace(replace(replace(ifnull(hostname, ''), '+', ''), '-',''), ' ', ''))
     ) as planet_id,
 
-    UPPER(replace(replace(replace(pl_name, '+', ''), '-',''), ' ', '')) as planet_id_without_star,
+    UPPER(replace(replace(replace(ifnull(pl_name, ''), '+', ''), '-',''), ' ', '')) as planet_id_without_star,
 
     CONCAT(
-            UPPER(REPLACE(disc_facility, ' ', '')), 
-            UPPER(REPLACE(disc_locale, ' ', ''))
+            UPPER(REPLACE(ifnull(disc_facility, ''), ' ', '')), 
+            UPPER(REPLACE(ifnull(disc_locale, ''), ' ', ''))
         ) as facility_id,
 
     concat(
-        UPPER(replace(hostname, ' ', '')), 
-        UPPER(spectype)
+        UPPER(replace(ifnull(hostname, ''), ' ', '')), 
+        UPPER(ifnull(spectype, ''))
     ) as star_id,
 
     UPPER(concat(
-        replace(pl_name, ' ', ''), 
-        replace(disc_facility, ' ', ''),
-        disc_year
+        replace(ifnull(pl_name, ''), ' ', ''), 
+        replace(ifnull(disc_facility, ''), ' ', ''),
+        ifnull(disc_year, 0)
     )) as detection_id,
 
     *
