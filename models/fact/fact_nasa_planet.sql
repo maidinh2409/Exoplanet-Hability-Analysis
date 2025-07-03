8ab0762aea75696415264594c8f18e835b400e73
Demi Le <lenguyenmaidinh2409@gmail.com>
2025-07-03 00:08:40 +0000

    update isHabitable in fact table

--- models/fact/fact_nasa_planet.sql
+++ models/fact/fact_nasa_planet.sql
@@ -17,8 +17,35 @@ join_table_hz as (
     left join {{ref("stg_hzgallery")}} as hz
     on hz.planet_id_without_star = nasa.planet_id_without_star
     order by nasa.P_HABITABLE desc
+),
+
+aggregate_cols as (
+    select
+        planet_id,
+        planet_id_without_star,
+        planet_name,
+        age,
+        star_id,
+        detection_id,
+        mass,
+        radius,
+        density,
+        t_eq,
+        flux,
+        distance_to_star,
+        period,
+        esi,
+        case
+            when (P_HABITABLE + isHabitable_hz) = 0 then 0
+            when (P_HABITABLE + isHabitable_hz) = 1 then 1
+            when P_HABITABLE = 1 AND isHabitable_hz = 1 then 1
+            when P_HABITABLE = 2 AND isHabitable_hz = 0 then 2
+            when P_HABITABLE = 2 AND isHabitable_hz = 1 then 3
+        end as isHabitable
+    from join_table_hz
 )
 
-select
+select 
     *
-from join_table_hz
+from aggregate_cols
+order by isHabitable desc
