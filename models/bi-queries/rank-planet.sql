74df736868cb8e0444730334cd1b077562f34876
Demi Le <lenguyenmaidinh2409@gmail.com>
2025-07-06 01:02:12 +0000

    -- Update rank-planet model

--- models/bi-queries/rank-planet.sql
+++ models/bi-queries/rank-planet.sql
@@ -4,6 +4,9 @@ earth_sim as (
     select
         planet_id,
         planet_name,
+        round(t_eq - 273.15, 2) AS t_eq,
+        round(density,2) as density,
+        cast(period as integer) as period,
         habitable_score,
         round(esi * 100, 1) as earth_similarity_score
     from {{ref('fact_nasa_planet')}}
